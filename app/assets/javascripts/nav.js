91418bd3f26d36703bf8ef60c19e299e4e6ae7e0
Niall Wallace <nj.wallace@outlook.com>
2015-09-18 16:05:42 +0100

    Added controllers and basic map functions

---
+++ app/assets/javascripts/nav.js
@@ -0,0 +1,9 @@
+console.log('hello Niall')
+
+
+var $navList = $('.nav-list');
+
+$navList.on('click', 'li:not(.selected)', function(e){
+  $navList.find(".selected").removeClass("selected");
+  $(e.currentTarget).addClass("selected");
+});
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,7 @@
 class PlacesController < ApplicationController
+
     def index
-    @places = Place.all
+    #@places = Place.all
   end
 
   def new
@@ -47,4 +48,6 @@ class PlacesController < ApplicationController
   def article_params
     params.require(:place).permit(:name, :long, :lat)
   end
+  
 end
+
